876fbc3827417775881ae952e53b10101f1e1e32
Kim Ashley Sunio <kimashleysunio@gmail.com>
2022-10-20 13:33:37 -1000

    Revert "removed forced login for now"
    This reverts commit 38ebda966d71e585097a7ee8096f8885940e59cc.

--- routes/agent-home.js
+++ routes/agent-home.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {    
+router.get('/', (req, res, next) => {
+    if (!req.isAuthenticated()) return res.redirect('/login');
+    
     res.render('agent-home');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
+    if (!req.isAuthenticated()) return res.redirect('/login');
+
     res.render('index', { accountType: req.user.type });
 });
 
