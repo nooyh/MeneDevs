9ce59fa852bd18e54ac6eed54c341297b645f19c
alohabeach <46428270+alohabeach@users.noreply.github.com>
2022-10-20 13:47:48 -1000

    fix errors

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index', { accountType: req.user.type });
+    res.render('index', { accountType: require?.user?.type || 'guest' });
 });
 
 module.exports = router;
